d1898bbb197f04b641c800b820a63b215c6e7947
Alfred Lysebraate <asl@knowit.no>
2010-08-13 11:51:13 +0200

    Generalized has-num-alive-neighbors

--- gol.clj
+++ gol.clj
@@ -1,10 +1,5 @@
 (load-file "./patterns.clj")
 
-#_ (defn to-bool [grid]
-	(vec (for [rows grid]
-		(vec (for [cell rows]
-			(if (= cell 1) true false))))))	
-
 (defn print-symbol [cell-value]
 	(if (= 1 cell-value) "#" " "))
 
@@ -19,7 +14,7 @@
 		(print-row row)))
 
 (defn get-cell [x y grid]
-	(nth (nth grid x nil) y nil))
+	(nth (nth grid x []) y 0))
 
 (defn has-life [[x y] grid]
 	(= 1 (get-cell x y grid)))
@@ -41,14 +36,13 @@
 (defn get-neighbors-values [x y grid]
 	(map (fn [[x1 y1]] (get-cell x1 y1 grid)) (get-neighbors-cord x y grid)))
 
-(defn has-num-alive-neighbors [neighbors num]
-	(= (count (filter #(= 1 %) neighbors)) num))
+(defn has-num-alive-neighbors [neighbors & nums]
+	(contains? (set nums) (reduce #(+ %1 %2) neighbors)))
 
 (defn evolve-single-cell [x y old-grid]
-	(let [neighbors-values (get-neighbors-values x y old-grid) 
-				has-three-neighbors (has-num-alive-neighbors neighbors-values 3)]
-	(if (has-life [x y] old-grid) (if (or has-three-neighbors (has-num-alive-neighbors neighbors-values 2)) 1 0)
-		(if has-three-neighbors 1 0))))
+	(let [neighbors-values (get-neighbors-values x y old-grid)]
+	(if (has-life [x y] old-grid) (if (has-num-alive-neighbors neighbors-values 2 3) 1 0)
+		(if (has-num-alive-neighbors neighbors-values 3) 1 0))))
 
 (defn evolve [grid]
 	(map-indexed (fn [i row] 
